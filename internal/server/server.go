5607a45f4814639fc2af17f03de6071fae5b6a51
Gabriel Felipe Kuhn Schiestl <gfkschiestl@minha.fag.edu.br>
2025-06-25 08:45:55 -0300

    feat: implements CORS

--- internal/server/server.go
+++ internal/server/server.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"github.com/Gabriel-Schiestl/api-go/internal/server/middlewares"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -10,6 +11,13 @@ var Router *gin.Engine
 func init() {
 	Router = gin.New()
 
+	config := cors.DefaultConfig()
+	config.AllowOrigins = []string{"http://localhost:3000"}
+	config.AllowMethods = []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"}
+	config.AllowHeaders = []string{"Origin", "Content-Type", "Accept", "Authorization"}
+	config.AllowCredentials = true
+
 	Router.Use(gin.Recovery())
 	Router.Use(middlewares.AuthMiddleware())
+	Router.Use(cors.New(config))
 }
